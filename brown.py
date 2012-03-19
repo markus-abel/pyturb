059cc370a80649aa097d7cdbd4465bbe34be58b0
Markus Abel <markus.w.abel@gmail.com>
2012-03-19 17:13:58 +0100

    changes to brown.py file

--- brown.py
+++ brown.py
@@ -5,10 +5,12 @@
 # mu: drift terms (moving average or long-term mean for stock returns)
 # S0: initial stock price
 
-from numpy.random import standard_normal
-from numpy import array, zeros, sqrt, shape
-from pylab import *
+ #numpy import array, zeros, sqrt, shape
+#from numpy.random import *
+#romom pylab import *
 
+import numpy as np
+import pylab as pl
 
 # Random walk:
 # define a timestep dt
@@ -22,18 +24,22 @@ from pylab import *
 
 # define the step in time
 
-def Brown():
+#def Brown():
 
-	Steps=round(T/dt); #Steps in years
-	S = zeros([N_Sim, Steps], dtype=float)
-	x = range(0, int(Steps), 1)
-	
-	for j in range(0, N_Sim, 1):
-	        S[j,0]= S0
-	        for i in x[:-1]:
-	                S[j,i+1]=S[j,i]+S[j,i]*(mu-0.5*pow(sigma,2))*dt+sigma*S[j,i]*sqrt(dt)*standard_normal();
-	        plot(x, S[j])
+  #Steps=round(T/dt); #Steps in years
+  #S = zeros([N_Sim, Steps], dtype=float)
+  #x = range(0, int(Steps), 1)
+  
+  #for j in range(0, N_Sim, 1):
+          #S[j,0]= S0
+          #for i in x[:-1]:
+                  #S[j,i+1]=S[j,i]+S[j,i]*(mu-0.5*pow(sigma,2))*dt+sigma*S[j,i]*sqrt(dt)*standard_normal();
+          #plot(x, S[j])
 
+def step(x,dt):
+  x = x + np.sqrt(dt)*np.random.normal()
+  return x
+ 
 # main program start
 # initialize 
 t0=0.
@@ -44,23 +50,38 @@ print N
 sigma = 0.5
 mu = 0.
 N_Part = 3
-x=zeros( [N_part,N], dtype=float )
+# state of the system
+x=np.zeros( [N_Part,N], dtype=float )
 print x
 
-# first method: run each particle from 0 to T
-# start again and run etc.
-n = 0
-while n < N_Sim:
-	n = n + 1
-	Brown()
 
 # second method: evolve all Particles from t to t+dt
-t=t0
-while t<T:
-  step(x,t)
+#t=t0
+## for loop
+## 
+#for i in range(0,T,N):
+  #t=t0+i*dt
+  #step()
+  
+# here define set of all times
+# initialize var, mean
 
-title('Brownian motion representation for N particles', N_Sim)
-xlabel('steps')
-ylabel('stock price')
-show()
+# while loop
+t=t0
+n=0
+while t<(T-dt):
+  print n,x[:,n]
+  x[:,n+1]=step(x[:,n],dt)
+  #pl.plot(x[:,n])
+  #pl.show()
+  # calculate mean instantaneous
+  # calculate instantaneous variance
+  t=t+dt
+  n = n+1
+
+# plot var and mean vs. time
+
+pl.plot(time,var)
+
+#show()
 
